4868c73ee11b9a8066d407d11548d87ea56df063
thisiskhandev <hassankhan427726@gmail.com>
2023-08-15 01:45:31 +0500

    Including subviews - Template II

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer style="border-top: 8px solid #ddd; padding: 20px; margin-top: 3rem;">
+    <h2 style="text-align: center">Footer</h2>
+</footer>
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
-</head>
-<body>
+@php
+    $fruits = ['one' => 'Apple','two' => 'Banana','three' =>  'Mango'];
+@endphp
+@includeWhen(true, "components.header", ['pageTitle' => 'Homepage', 'fruitNames' => $fruits])
+
     <h1>Homepage</h1>
     <a href="{{ route('about') }}">About</a>
     {{-- <a href="{{ route('posts') }}/11abc">Posts</a> --}}
     <a href="{{ route('posts', '123') }}/abc">Posts</a>
     <a href="{{ route('contact') }}">Contact Page</a>
-</body>
-</html>
+
+@include("components.footer")
