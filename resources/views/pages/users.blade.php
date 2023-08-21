75c3e8038a091b8e81c5464119ee032bc806a2b7
thisiskhandev <hassankhan427726@gmail.com>
2023-08-22 02:02:13 +0500

    Passing array data into view file

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -1,6 +1,39 @@
 @extends('layout.masterlayout')
 
+@section('pageTitle', 'Users')
 
 @section('content')
-<h1>TEST</h1>
+<h1>Users</h1>
+{{-- <h2>{{$user}}</h2>
+<h2>{{!empty($city) ? $city : "No city found!"}}</h2> --}}
+
+{{-- <div>
+    <ul>
+        @foreach ($user as $id => $userdata)
+        <li>{{$userdata['name']}} | {{$userdata['phone']}} | {{$userdata['city']}}</li>
+@endforeach
+</ul>
+</div> --}}
+
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">City</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($user as $id => $userdata)
+        <tr>
+            <th scope="row">{{$id + 1}}</th>
+            <td>{{$userdata['name']}}</td>
+            <td>{{$userdata['phone']}}</td>
+            <td>{{$userdata['city']}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,39 @@ Route::get('/contact', function () {
 
 Route::get('/users', function () {
     $user_one = 'Hassan Khan';
-    return view('pages.users', ['user' => $user_one]);
+    // METHOD 1
+    // return view('pages.users', [
+    //     'user' => $user_one,
+    //     'city' => 'Karachi'
+    // ]);
+
+    // METHOD 2
+    // return view('pages.users')
+    // ->with('user', $user_one)
+    // ->with('city', 'Islamabad');
+
+    // METHOD 3
+    // return view('pages.users')->withUser($user_one)->withCity('Lahore');
+
+    // return view('pages.users', [
+    //     'user' => $user_one,
+    //     'city' => ''
+    // ]);
+
+    $usersData = [
+        ['name' => 'Khan', 'phone' => 99875641231, 'city' => 'Karachi'],
+        ['name' => 'Smith', 'phone' => 1234567890, 'city' => 'New York'],
+        ['name' => 'Garcia', 'phone' => 9876543210, 'city' => 'Los Angeles'],
+        ['name' => 'Johnson', 'phone' => 5551234567, 'city' => 'Chicago'],
+        ['name' => 'Liu', 'phone' => 8876543210, 'city' => 'San Francisco'],
+        ['name' => 'Kim', 'phone' => 1239874560, 'city' => 'Seoul'],
+        ['name' => 'Martinez', 'phone' => 7778889999, 'city' => 'Mexico City'],
+        ['name' => 'Chen', 'phone' => 6665554444, 'city' => 'Shanghai'],
+        ['name' => 'Brown', 'phone' => 5558887777, 'city' => 'London'],
+        ['name' => 'Gonzalez', 'phone' => 9876543210, 'city' => 'Madrid'],
+    ];
+
+    return view('pages.users', ['user' => $usersData]);
 })->name('users');
 
 Route::redirect('/about-us', 'about', 301);
