ead1b840d58676432f26d8f6dbe7cceed708196a
thisiskhandev <hassankhan427726@gmail.com>
2023-09-07 02:05:15 +0500

    Migration minor issue fixed in while reseting migrate

--- database/migrations/2023_09_06_203756_drop_users_table.php
+++ database/migrations/2023_09_06_203756_drop_users_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::rename('student', 'students');
+        Schema::dropIfExists('users');
     }
 
     /**
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        //
     }
 };
