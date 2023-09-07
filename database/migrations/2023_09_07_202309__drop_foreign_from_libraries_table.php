a82e782084177f3a135a137573721da63bdf8c7a
thisiskhandev <hassankhan427726@gmail.com>
2023-09-08 01:28:23 +0500

    16 - Migration Primary & Foreign key

---
+++ database/migrations/2023_09_07_202309__drop_foreign_from_libraries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('libraries', function (Blueprint $table) {
+            $table->dropForeign(['stu_id']);
+            $table->dropColumn('stu_id'); // Remember!! You cannot delete direct foreign key column until you haven't drop it's foreign constraint. First always use dropForeign before using dropColumn of the foreign column.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('libraries', function (Blueprint $table) {
+            //
+        });
+    }
+};
