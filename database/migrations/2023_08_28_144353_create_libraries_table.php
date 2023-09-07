d579ca580020129ce3e0a1c18ebd27d31e06ad28
thisiskhandev <hassankhan427726@gmail.com>
2023-09-08 01:18:29 +0500

    Better approach to use the foreign and cascade constraints

--- database/migrations/2023_08_28_144353_create_libraries_table.php
+++ database/migrations/2023_08_28_144353_create_libraries_table.php
@@ -15,14 +15,22 @@ return new class extends Migration
     {
         Schema::create('libraries', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('stu_id'); // This will work for normal id() not for integer()
+            // $table->unsignedBigInteger('stu_id'); // This will work for normal id() not for integer()
+
             // $table->integer('stu_id');
             // $table->bigIncrements('stu_id');
-            $table->foreign('stu_id')
-                ->references('id')
-                ->on('students')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');
+
+            // $table->foreign('stu_id')
+            //     ->references('id')
+            //     ->on('students')
+            //     ->onUpdate('cascade')
+            //     ->onDelete('cascade');
+
+            // $table->foreignId('student_id')->constrained() // It will search the table name student where after underscore search for the name of id and creates a student_id as a foreign key.
+            //     ->cascadeOnUpdate()->cascadeOnDelete();
+
+            $table->foreignId('stu_id')->constrained('students') // ForeignId creates the foreign column with the desired name and constrained with name finds the primary from the selected table.
+                ->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('book');
             $table->date('due_date')->nullable();
             $table->boolean('status');
