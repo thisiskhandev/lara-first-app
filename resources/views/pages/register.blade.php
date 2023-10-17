da131584813c3a39e87b952c389c836b8b16c2fe
Hassan Khan <hassan.khan@lslogics.local>
2023-10-17 22:00:11 +0500

    Working on Register view

---
+++ resources/views/pages/register.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.masterlayout')
+
+@section('pageTitle', 'Login')
+
+@section('content')
+<h1>Register</h1>
+
+<section>
+    <form action="login" method="POST" id="login" class="my-4">
+        @csrf
+        <div class="mb-3">
+            <input type="text" name="username" class="form-control" id="" placeholder="Username" required />
+        </div>
+        <div class="mb-3">
+            <input type="email" name="email" class="form-control" id="" placeholder="Email address" required />
+        </div>
+        <div class="mb-3">
+            <input type="password" name="passpassword" class="form-control" id="" placeholder="Password" required />
+        </div>
+        <button type="submit" class="btn btn-outline-success">Submit</button>
+    </form>
+</section>
+
+@endsection
