fcbaa94cca2cb2945ee4f3075d67f4e8cab12f3f
Hassan Khan <hassan.khan@lslogics.local>
2023-11-01 22:35:43 +0500

    Condition added on sidebar links

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -5,8 +5,10 @@
     <ul class="list-group list-group-flush list-inline">
         <li class="list-group-item"><a href="{{route('home')}}">Home</a></li>
         <li class="list-group-item"><a href="{{route('students')}}">Students</a></li>
+        @if (empty(Auth::user()->name))
         <li class="list-group-item"><a href="{{route('register')}}">Register</a></li>
         <li class="list-group-item"><a href="{{route('login')}}">Login</a></li>
+        @endif
         <li class="list-group-item"><a href="{{route('contact')}}">Contact Us</a></li>
     </ul>
 </div>
